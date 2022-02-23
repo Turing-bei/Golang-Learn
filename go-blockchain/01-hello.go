6456c6af7bff07a4835332599e2b6e114e1ba220
Turingbei <577390480@qq.com>
2022-02-23 22:54:11 +0800

    01-初识Go语言

---
+++ go-blockchain/01-hello.go
@@ -0,0 +1,15 @@
+// 我是注释
+/*
+ * 我还是注释
+ */
+
+package main // 包名
+
+// 导入包
+import ( 
+	"fmt"  // 官方提供的包，标准化输出、格式化字符串等功能尽在掌握
+)
+
+func main()  {	// 主函数
+	fmt.Println("hello world")	// 打印 hello world，并输出换行
+}
